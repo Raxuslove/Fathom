3b4cc97922247be4a7e23942facd81c74ce8bdcc
Rax <183243579+Raxuslove@users.noreply.github.com>
2026-08-14 19:03:10 +0200

    Replace framed UI vignettes with smooth gradients

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
   forgotten/optional image cannot abort the entire service-worker install and
   strand the installed PWA on an older cache.
 */
-const CACHE_NAME = "lowfathom-v0.101.10";
+const CACHE_NAME = "lowfathom-v0.101.11";
 
 const CORE_SHELL = [
   "./",
