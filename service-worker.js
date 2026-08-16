ee98c2ce4eb4e34c8b981cac65aef9298b1999e7
Rax <183243579+Raxuslove@users.noreply.github.com>
2026-08-16 11:27:19 +0200

    v0.106.5: add combat damage popups and mobile scroll safety

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
   forgotten/optional image cannot abort the entire service-worker install and
   strand the installed PWA on an older cache.
 */
-const CACHE_NAME = "lowfathom-v0.106.4";
+const CACHE_NAME = "lowfathom-v0.106.5";
 
 const CORE_SHELL = [
   "./",
