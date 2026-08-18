d4206a7b504126699229796efc7a35f2c9602bd8
Rax <183243579+Raxuslove@users.noreply.github.com>
2026-08-19 00:09:45 +0200

    v0.111.2: Combat Rhythm

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
   forgotten/optional image cannot abort the entire service-worker install and
   strand the installed PWA on an older cache.
 */
-const CACHE_NAME = "lowfathom-v0.111.0";
+const CACHE_NAME = "lowfathom-v0.111.2";
 
 const CORE_SHELL = [
   "./",
