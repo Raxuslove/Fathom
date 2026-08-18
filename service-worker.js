e40096dd4b5e2a5fed594f597ee827e80c13625a
Rax <183243579+Raxuslove@users.noreply.github.com>
2026-08-19 00:29:29 +0200

    v0.111.3: CON protection baseline fix

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
   forgotten/optional image cannot abort the entire service-worker install and
   strand the installed PWA on an older cache.
 */
-const CACHE_NAME = "lowfathom-v0.111.2";
+const CACHE_NAME = "lowfathom-v0.111.3";
 
 const CORE_SHELL = [
   "./",
