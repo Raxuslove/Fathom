fc7615f13cc0f8cdfd1fdd33dcdb8c55bb086ee3
Rax <183243579+Raxuslove@users.noreply.github.com>
2026-08-18 16:25:30 +0200

    v0.111.0: combat timeline v2

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
   forgotten/optional image cannot abort the entire service-worker install and
   strand the installed PWA on an older cache.
 */
-const CACHE_NAME = "lowfathom-v0.110.5";
+const CACHE_NAME = "lowfathom-v0.111.0";
 
 const CORE_SHELL = [
   "./",
