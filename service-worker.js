f5c67af74aa879c67312c322f2dfd4eafb3c6d4a
Rax <183243579+Raxuslove@users.noreply.github.com>
2026-08-18 10:48:14 +0200

    v0.110.0: alternating turn combat and Protection flow

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
   forgotten/optional image cannot abort the entire service-worker install and
   strand the installed PWA on an older cache.
 */
-const CACHE_NAME = "lowfathom-v0.109.1";
+const CACHE_NAME = "lowfathom-v0.110.0";
 
 const CORE_SHELL = [
   "./",
